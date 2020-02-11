9854396c25e77fe2384d72bfbdaf1c2d91b5db40
chee <rails@snoot.club>
2020-02-11 14:04:32 +0000

    update

--- app/controllers/bookys_controller.rb
+++ app/controllers/bookys_controller.rb
@@ -5,11 +5,11 @@ class BookysController < ApplicationController
   def create
     pdf = params[:pdf]
     basename = File.basename(pdf.path)
-    Dir.chdir('/tmp')
-    `/usr/chee/projects/booky/make-booky #{basename}`
-    File.open("/tmp/#{basename}.booky.pdf") do |file|
-      puts file
-      send_data file.read.force_encoding('BINARY'), filename: "#{pdf.original_filename}.booky.pdf", type: 'application/pdf', disposition: 'attachment'
+    Dir.mktmpdir do |dir|
+        `/bin/make-booky #{pdf.path}`
+        File.open("#{basename}.booky.pdf") do |file|
+            send_data file.read.force_encoding('BINARY'), filename: "#{pdf.original_filename}.booky.pdf", type: 'application/pdf', disposition: 'attachment'
+        end
     end
   end
 end
