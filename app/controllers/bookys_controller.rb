81910140da21ccb7704b5c2eeb6fb42a493895c0
chee <rails@snoot.club>
2020-02-11 15:01:32 +0000

    Update to use better filenames in public

--- app/controllers/bookys_controller.rb
+++ app/controllers/bookys_controller.rb
@@ -4,12 +4,14 @@ class BookysController < ApplicationController
 
   def create
     pdf = params[:pdf]
-    basename = File.basename(pdf.path)
-    Dir.mktmpdir do |dir|
-        `/bin/make-booky #{pdf.path}`
-        File.open("#{basename}.booky.pdf") do |file|
-            send_data file.read.force_encoding('BINARY'), filename: "#{pdf.original_filename}.booky.pdf", type: 'application/pdf', disposition: 'attachment'
-        end
+    basename = File.basename(pdf.path, ".pdf")
+    Dir.chdir('public')
+    `/bin/make-booky #{pdf.path}`
+    bookyname = "#{basename}.booky.pdf"
+    outname = "#{File.basename(pdf.original_filename, ".pdf")}.booky.pdf"
+    File.rename(bookyname, outname)
+    File.open(outname) do |file|
+        send_data file.read.force_encoding('BINARY'), filename: outname, type: 'application/pdf', disposition: 'attachment'
     end
   end
 end
